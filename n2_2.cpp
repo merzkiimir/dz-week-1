934d49b015a96a0fa065b291da707bdaeebe4a7d
merzkiimir <merzkiimir@mail.ru>
2024-09-15 15:15:49 +0300

    Create n2_2.cpp

---
+++ n2_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <cmath>
+/* programm 2.3
+H = visota
+V = skorost'
+g = const
+a_g = ugol v gradusah
+a = ugol v radianah
+x1 = dal'nost' poleta
+T = vremya poleta
+
+*/
+
+
+int main(){
+    int H, V, a_g;
+    float g = 9.8;
+    std::cout << "Vvedite znachenie H(visota):\n";
+    std::cin >> H;
+    std::cout << "Vvedite znachenie V(skorost'):\n";
+    std::cin >> V;
+    std::cout << "Vvedite znachenie a(angle):\n";
+    std::cin >> a_g;
+    double a = a_g * M_PI / 180;
+    double T = ((V * sin(a))/g)*(1 + sqrt(1 + ((2 * g * H)/(pow(V, 2)*pow(sin(a), 2)))));
+    std::cout << "Vremya poleta T = " << T << "\n";
+    float x1 = V * T * cos(a);
+    std::cout << "x(T) = " << x1 << "\n";
+    std::cout << "Vvedite znachenie t(moment vremeni):\n";
+    int t;
+    std::cin >> t;
+    float x2 = V * t * cos(a);
+    float y2 = H + V * t * sin(a) - 1/2 * g * pow(t, 2);
+    std::cout << "x(t) = " << x2 << " , y(t) = " << abs(y2) << "\n";
+    return 0;
+}
