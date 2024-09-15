2017af92d0e629d70e1cb00024ba681b2d87ec7c
merzkiimir <merzkiimir@mail.ru>
2024-09-15 23:54:54 +0300

    Update n2_2.cpp

--- n2_2.cpp
+++ n2_2.cpp
@@ -3,36 +3,37 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 /* programm 2.3
-H = visota
-V = skorost'
+H = высота
+V = скорость
 g = const
-a_g = ugol v gradusah
-a = ugol v radianah
-x1 = dal'nost' poleta
-T = vremya poleta
+a_g = угол в градусах
+a = угол в радианах
+x1 = дальность полета
+T = время полета
 
 */
 
 
 int main(){
+    setlocale(LC_ALL, "Russian");
     int H, V, a_g;
     float g = 9.8;
-    std::cout << "Vvedite znachenie H(visota):\n";
+    std::cout << "введите значение H(высота):\n";
     std::cin >> H;
-    std::cout << "Vvedite znachenie V(skorost'):\n";
+    std::cout << "введите значение V(скорость):\n";
     std::cin >> V;
-    std::cout << "Vvedite znachenie a(angle):\n";
+    std::cout << "введите значение a(угол):\n";
     std::cin >> a_g;
     double a = a_g * M_PI / 180;
     double T = ((V * sin(a))/g)*(1 + sqrt(1 + ((2 * g * H)/(pow(V, 2)*pow(sin(a), 2)))));
-    std::cout << "Vremya poleta T = " << T << "\n";
+    std::cout << "время полета T = " << T << "\n";
     float x1 = V * T * cos(a);
     std::cout << "x(T) = " << x1 << "\n";
-    std::cout << "Vvedite znachenie t(moment vremeni):\n";
+    std::cout << "введите значение t(момент времени):\n";
     float t;
     std::cin >> t;
     float x2 = V * t * cos(a);
     float y2 = H + (V * t * sin(a)) - 0.5 * g * pow(t, 2);
-    std::cout << "x(t) = " << x2 << " , y(t) = " << y2 << " or rasstoyanie y(t) ~ " << abs(y2 - H);
+    std::cout << "x(t) = " << x2 << " , y(t) = " << y2 << " или расстояние y(t) ~ " << abs(y2 - H);
     return 0;
 }
