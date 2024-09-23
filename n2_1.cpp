a925c1f281ad27dafaca054cb7a0e1bffda0563d
merzkiimir <merzkiimir@mail.ru>
2024-09-23 10:31:47 +0300

    Update n2_1.cpp

--- n2_1.cpp
+++ n2_1.cpp
@@ -8,18 +8,22 @@ k = k
 omega = w
 T = T
 fi = fi 
+a = A
 */
 
 
 int main(){
     setlocale(LC_ALL, "Russian");
-    int k, omega;
+    int k, omega, a;
     std::cout << "введите значение k:\n";
     std::cin >> k;
     std::cout << "введите значение w(omega):\n";
     std::cin >> omega;
+    std::cout << "введите значение A:\n";
+    std::cin >> a;
     double fi = asin(1/k);
     double T = (M_PI/2 - fi)/omega;
-    std::cout << "T = " << T;
+    std::cout << "T = " << T << "\n";
+    std::cout << "x(T) = " << a; //при t = T sin = 1 => x(T) = A
     return 0;
 }
